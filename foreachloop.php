ddb5aa3ab54aeba966a2e3ff9db26bc9c60c3a95
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-21 22:21:21 +0530

    foreachloop

---
+++ foreachloop.php
@@ -0,0 +1,17 @@
+<?php
+//$arr=[
+    //"HTML"=>2500,
+    //"CSS"=>2519,
+    //"JS"=>3000,
+  //  "PHP"=>4000,
+
+//];
+//echo "<pre>";
+//print_r($arr)
+//echo "</pre>";
+
+
+foreach($arr as $course => $fees){
+    echo "the fees of $course is $fees\n";
+}
+?>
